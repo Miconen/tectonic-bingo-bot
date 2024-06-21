4251fcf2b7fb8f7538430a97b3a3c31d7eb00336
Miconen <micropizza.mc@gmail.com>
2024-06-21 19:56:34 +0300

    Added special hyphen to list of stripped characters

--- bot/utils/input.py
+++ bot/utils/input.py
@@ -1,5 +1,5 @@
 def sanitize_string(s: str):
-    special_chars = [" ", "-", "_", ".", ",", "!", "?", ":", ";", "'", '"', "`"]
+    special_chars = [" ", "-", "_", ".", ",", "!", "?", ":", ";", "'", '"', "`", "’"]
 
     for i in range(len(special_chars)):
         s = s.replace(special_chars[i], "")
