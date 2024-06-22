ad53f264fed85b2409cbc19896e070113b51d6a6
Miconen <micropizza.mc@gmail.com>
2024-06-22 05:01:42 +0300

    Defer response of board to minimize timeouts

--- bot/commands/board.py
+++ bot/commands/board.py
@@ -19,26 +19,33 @@ class Board(commands.Cog):
     @app_commands.command(name="board", description="View the board of a team.")
     async def board(self, i: discord.Interaction):
         """View the board of a team."""
+        await i.response.defer()
+
         if i.channel is None:
-            return
+            res = "Submission channel is not available."
+            return await i.followup.send(res, ephemeral=True)
+
         if not isinstance(i.channel, discord.TextChannel):
-            return
+            res = "Submission channel is not a text channel."
+            return await i.followup.send(res, ephemeral=True)
+
         if not isinstance(i.user, discord.Member):
-            return
+            res = f"Command not available in DMs."
+            return await i.followup.send(res, ephemeral=True)
 
         team = in_team(i.user, state.teams)
         if team is None:
             res = f"You are not in a bingo team, if you think this is a mistake, please contact an admin"
-            return await i.response.send_message(res, ephemeral=True)
+            return await i.followup.send(res, ephemeral=True)
 
         team_name = state.teams[team].get_name()
 
         image = images.get_image(team)
         if not image:
             res = f"Board for {team_name} is not available."
-            return await i.response.send_message(res)
+            return await i.followup.send(res)
 
-        await i.response.send_message(f"Board for {team_name}.", files=[image])
+        await i.followup.send(f"Board for {team_name}.", files=[image])
 
 
 async def setup(bot: commands.Bot) -> None:
